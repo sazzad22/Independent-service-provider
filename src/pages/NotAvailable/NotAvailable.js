4d640227a793174c0c5243467b52c99d65ce444a
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-17 14:12:33 +0600

    components added

---
+++ src/pages/NotAvailable/NotAvailable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotAvailable = () => {
+    return (
+        <div>
+            <h1>Not Available!!</h1>
+        </div>
+    );
+};
+
+export default NotAvailable;
