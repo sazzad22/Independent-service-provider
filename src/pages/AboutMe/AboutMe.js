1066569be74358371acc5895624111af73e6e292
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-18 18:05:18 +0600

    about me page updated

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -3,7 +3,8 @@ import React from 'react';
 const AboutMe = () => {
     return (
         <div>
-            AboutMe
+            <h2>Sazzad Ur Rahman</h2>
+            <h4>I am an Engineering Student and creator of this very website. I and learning to be a web developer. I love coding and my goal is to write good and efficient code . I want to solve my problems along with other's problems. And With the permission of Allah ,the merciful, I am ready to work hard for it.</h4>
         </div>
     );
 };
