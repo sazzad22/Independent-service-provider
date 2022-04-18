45cdc465c7d3ddfc5488e5c3b786880fd5517622
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-18 17:28:58 +0600

    error handled and reset pasword(toast) added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -20,8 +20,8 @@ const Banner = () => {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Welcome to Lens Guru</h3>
+          <p>I am Jason Hoble ,a wild life photographer.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -32,8 +32,8 @@ const Banner = () => {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Photography is my passion</h3>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -44,9 +44,9 @@ const Banner = () => {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Feel free to brows through the website</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            I provide other services than wild life photography.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -2,49 +2,78 @@ import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import SocialLogin from "../SocialLogin/SocialLogin";
-import auth from '../../../firebase.init';
-import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
   const location = useLocation();
   const navigate = useNavigate();
+  let errorElement;
+
 
   let from = location.state?.from?.pathname || "/";
-  
 
-  const [
-    signInWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useSignInWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  //reset password auth
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
   //redirecting to the page from where he came from
   if (user) {
     navigate(from, { replace: true });
   }
-  
-  const handleSubmit = event => {
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
     signInWithEmailAndPassword(email, password);
-}
+  };
 
   const navigateToSignup = () => {
-        navigate('/signup');
-  }
-  
+    navigate("/signup");
+  };
+
+  //email or password error
+  if (error) {
+    console.log(error);
+    errorElement = <p className='text-danger'>Error: {error?.message}</p>
+}
+
+  //reset password handle
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast("Reset password email sent");
+    } else {
+      toast("please enter your email address");
+    }
+  };
+
   return (
     <div className=" mx-auto container w-50 min-vh-100">
       <h3>Please Login</h3>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter email"  required/>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -52,16 +81,44 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
-        
+
         <Button variant="primary" type="submit">
           Login
-              </Button>
-              <p>New to the website? <Link to="/signup" className='text-primary  pe-auto ' onClick={navigateToSignup} >Please Sign Up</Link></p>
+        </Button>
+
+        {errorElement}
+
+        <p>
+          New to the website?{" "}
+          <Link
+            to="/signup"
+            className="text-primary  pe-auto "
+            onClick={navigateToSignup}
+          >
+            Please Sign Up
+          </Link>
+        </p>
+
+        <p>
+          Forget Password?{" "}
+          <button
+            className="btn btn-link text-primary pe-auto "
+            onClick={resetPassword}
+          >
+            Reset Password
+          </button>{" "}
+        </p>
       </Form>
 
       <SocialLogin></SocialLogin>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
