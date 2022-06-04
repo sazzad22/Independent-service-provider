ce5044315f30b5518f2d03e4a028928a2e08307e
Sazzad ur Rahman <22joylobo@gmail.com>
2022-06-04 07:43:20 +0600

    about update

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => {
     return (
         <div className='w-75 p-5 mx-auto min-vh-100 '>
             <h2 className='text-center text-secondary'>Sazzad Ur Rahman</h2>
-            <h5>I am an engineering Student and creator of this very website. I am learning to be a web developer. I love coding and my goal is to write good and efficient code . I want to solve my problems along with other's problems. And With the permission of Allah ,the merciful, I am ready to work hard for it.</h5>
+            <h5>I am an engineering Student and developer of this very website. I am learning to be a web developer. I love coding and my goal is to write good and efficient code . I want to solve my problems along with other's problems. And With the permission of Allah ,the merciful, I am ready to work hard for it.</h5>
         </div>
     );
 };
